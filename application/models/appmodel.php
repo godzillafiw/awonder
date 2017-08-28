2f5b81300ce7e7a70220d80045e5f696f3ec1631
DESKTOP-CVH75MR\Godzillafiw <godzillafiw@gmail.com>
2017-08-29 00:11:55 +0700

    แสดงข้อมูลหน้าแดชบอร์ด

--- application/models/appmodel.php
+++ application/models/appmodel.php
@@ -2,47 +2,52 @@
 
 class AppModel extends CI_Model {
 
-    public function __construct()
-     {
-      parent::__construct();
-     }
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    function get_all_records($table,$where,$join_table,$join_criteria,$order,$limit=NULL)
-		{
-			$this->db->where($where);
-			if($join_table){
+	function get_all_records($table,$where,$join_table,$join_criteria,$order,$limit=NULL)
+	{
+		$this->db->where($where);
+		if($join_table){
 			$this->db->join($join_table,$join_criteria);
-			}
-			$query = $this->db->order_by($order,'asc')->get($table,$limit);
-			if ($query->num_rows() > 0){
-				return $query->result();
-			} else{
-				return NULL;
-			}
 		}
+		$query = $this->db->order_by($order,'asc')->get($table,$limit);
+		if ($query->num_rows() > 0){
+			return $query->result();
+		} else{
+			return NULL;
+		}
+	}
 
-	function users(){
+	function users()
+	{
 		return $this->db->get('aw_users')->result();
 	}
 
-	function categories(){
+	function categories()
+	{
 		return $this->db->get('aw_categories')->result();
 	}
 
-	function payments(){
+	function payments()
+	{
 		return $this->db->get('payment_methods')->result();
 	}
 
-	function user_by_id($id){
+	function user_by_id($id)
+	{
 		return $this->db-> where(array('id'=>$id))->get('users')->result();
 	}
 
 	function insert($table,$data){
-			$this->db->insert($table, $data);
-			return $this->db->insert_id();
+		$this->db->insert($table, $data);
+		return $this->db->insert_id();
 	}
 
-	function update($table,$data,$where){
+	function update($table,$data,$where)
+	{
 		$this->db->where($where)->update($table, $data);
 		return TRUE;
 	}
@@ -52,10 +57,15 @@ class AppModel extends CI_Model {
 		//$array = array('project_title' => $keyword, 'project_code' => $keyword);
 		$this->db->like('project_title',$keyword); 
 		return $this->db->order_by('date_created','desc')	
-						->where($where)					
-						->get('projects')->result();
+		->where($where)					
+		->get('projects')->result();
+	}
+
+	public function count_table($table)
+	{
+		return $this->db->count_all($table);
 	}
 }
-     
-     /* End of file appmodel.php */
-     /* Location: ./application/models/appmodel.php */ 
+
+/* End of file appmodel.php */
+/* Location: ./application/models/appmodel.php */ 
--- application/modules/admin/controllers/Admin.php
+++ application/modules/admin/controllers/Admin.php
@@ -9,6 +9,12 @@ class Admin extends ADMIN_Controller {
 	
 	public function index()
 	{
+		$data['total_products']		=	$this->AppModel->count_table('aw_product');
+		$data['total_quotations']	=	$this->AppModel->count_table('aw_quotation');
+		$data['total_contacts']		=	$this->AppModel->count_table('aw_contacts');
+		$data['total_orders']		=	$this->AppModel->count_table('aw_orders');
+		$data['total_users']		=	$this->AppModel->count_table('system_users');
+		$data['total_category']		=	$this->AppModel->count_table('aw_categories');
 		$this->template->set_layout('temp_back')
 		->build('dashboard/dashboard_view',isset($data) ? $data : NULL);
 	}
